914da2e8d47b79b17a249c55d879dcf98291c8a1
Andrew Dunkman <adunkman@gmail.com>
2018-04-09 18:09:24 -0400

    Design tweaks.

--- website/index.js
+++ website/index.js
@@ -14,19 +14,28 @@ if (serviceRequestNumber) {
 
       if (r.isClosed()) {
         const duration = humanizeDuration(r.closedAt() - r.requestedAt(), options)
-        status = `Closed after ${duration} at ${r.closedAt().toLocaleString()}.`
+        status = `Completed after <time datetime="${r.closedAt().toISOString()}" title="${r.closedAt().toLocaleString()}">${duration}</time>.`
       }
       else {
         const duration = humanizeDuration(new Date() - r.requestedAt(), options)
-        status = `In progress, opened ${duration} ago at ${r.requestedAt().toLocaleString()}.`
+        status = `In progress for <time datetime="${r.requestedAt().toISOString()}" title="${r.requestedAt().toLocaleString()}">${duration}</time>.`
       }
 
       document.querySelector(".js-output").innerHTML = `
-        <h1>Service Request ${r.number()}</h1>
-        <p>${status}</p>
-        <p>Reported to ${r.organization()}, ${r.department()} for ${r.type()}.</p>
-        <p>
-          ${r.hasAddress() ? r.addressLines().join("<br>") + "<br>" : ""}
+        <h1>${r.number()}</h1>
+
+        <div class="section status-text">
+          ${status}
+        </div>
+
+        <div class="section request-type">
+          <div class="request-type">${r.type()}</div>
+          <div class="assignee">${r.organization()}, ${r.department()}</div>
+        </div>
+
+        <div class="section map">
+          ${r.hasAddress() ? '<div class="address">' + r.addressLines().join("<br>") + "</div>" : ""}
+
           <a href="https://www.google.com/maps/search/?${new URLSearchParams({
             api: 1,
             query: r.coordinates()
@@ -35,10 +44,15 @@ if (serviceRequestNumber) {
               center: r.coordinates(),
               markers: r.coordinates(),
               size: "600x300",
-              zoom: "15"
+              scale: 2,
+              zoom: 15
             })}">
           </a>
-        </p>
+        </div>
+
+        <div class="section home">
+          <a href="/">Find Another Service Request</a>
+        </div>
       `
     })
 }

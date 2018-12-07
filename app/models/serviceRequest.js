5d3555e40ee516a8de3d4135c069c4766da84b38
Andrew Dunkman <adunkman@gmail.com>
2018-12-06 22:32:26 -0500

    Add service request estimates when available.
    This data was exported from https://dc311api.herokuapp.com/v2/services.json — I’d like to use that API directly, but it is undocumented.

--- app/models/serviceRequest.js
+++ app/models/serviceRequest.js
@@ -1,5 +1,6 @@
 import timezone from "timezone"
 import tzdata from "timezone/America/New_York"
+import services from "../../db/static/services"
 
 const tz = timezone(tzdata)
 
@@ -19,10 +20,18 @@ const toDate = (localizedUnixTimestamp) => {
 export default class ServiceRequest {
   constructor(attributes) {
     this.attributes = attributes
+    this.service = services.find((s) => {
+      return (
+        s.agency === this.organization &&
+        s.service_code === this.code &&
+        s.service_name === this.type
+      )
+    })
   }
 
   get isClosed() { return this.attributes.STATUS_CODE === "CLOSED" }
   get number() { return this.attributes.SERVICEREQUESTID }
+  get code() { return this.attributes.SERVICECODE }
   get type() { return this.attributes.SERVICECODEDESCRIPTION }
   get department() { return this.attributes.SERVICETYPECODEDESCRIPTION }
   get organization() { return this.attributes.ORGANIZATIONACRONYM }
@@ -43,6 +52,13 @@ export default class ServiceRequest {
         this.attributes.STREETADDRESS,
         `${this.attributes.CITY}, ${this.attributes.STATE} ${this.attributes.ZIPCODE}`
       ]
+    }
+  }
+
+  get hasEstimate() { return this.service && this.service.sla }
+  get estimate() {
+    if (this.hasEstimate) {
+      return `${this.service.sla} ${this.service.sla_type}`.toLowerCase()
     }
   }
 }
